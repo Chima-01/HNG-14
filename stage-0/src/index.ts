965631a693e0bf244455c4b6f1fd4ce23bfdc320
Chinemerem <divinechima76@gmail.com>
2026-04-15 04:53:34 +0100

    fix: correctly handle all api calls for edge cases

--- stage-0/src/index.ts
+++ stage-0/src/index.ts
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 const app = express();
 const PORT = 3000;
+const validateGender = ['male', 'female'];
 
  
 app.use(cors());
@@ -14,41 +15,39 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.get('/api/classify', async (req: Request, res: Response) => {
-  let name = req.query.name as string | undefined;
+  const name = req.query.name as string | undefined;
 
   if (!name) {
-    return res.status(400).json({ status: "error", error: "Missing or empty name parameter" 
+    return res.status(400).json({ status: "error", message: "Missing or empty name parameter" 
     });
   }
-
-  name = name?.replace(/[^a-zA-Z]/g, '');
+  let checkName = Array.isArray(name) ? String(name[0]) : String(name);
+  checkName = checkName?.replace(/[^a-zA-Z \-]/g, '');
   
-  if (typeof name !== 'string' || name.length === 0) {
-    return res.status(422).json({ status: "error", error: "name is not a string" });
+  if (typeof checkName !== 'string' || checkName.length === 0) {
+    return res.status(422).json({ status: "error", message: "name is not a string" });
   }
-  
   
   
   try {
     const response = await axios.get(
-      `https://api.genderize.io?name=${encodeURIComponent(name)}`,
-    { timeout: 500 }
+      `https://api.genderize.io?name=${encodeURIComponent(checkName)}`,
+    { timeout: 5000 }
     );
 
     if (!response.data || typeof response.data !== 'object') {
-      return res.status(502).json({ status: "error", message: "Invalid response from upstream API" });
+      return res.status(502).json({ status: "error", message: "Upstream or server failure" });
     }
     const { gender, probability: prob, count } = response.data;
 
     const probability =  prob !== undefined ? parseFloat(prob) : 0;
     const sample_size = count !== undefined ? parseInt(count, 10) : 0;
-    const validateGender = ['male', 'female'];
 
 
-    if (isNaN(probability) || isNaN(sample_size)) {
-  return res.status(500).json({ 
+  if (isNaN(probability) || isNaN(sample_size)) {
+    return res.status(500).json({ 
     status: "error", 
-    message: "Invalid data from gender API" 
+    message: "No prediction available for the provided name" 
   });
 }
 
@@ -61,7 +60,7 @@ app.get('/api/classify', async (req: Request, res: Response) => {
     return res.status(200).json({
       status: "success",
       data: {
-        name,
+        name: checkName,
         gender,
         probability,
         sample_size,
@@ -70,9 +69,18 @@ app.get('/api/classify', async (req: Request, res: Response) => {
       }
     });
   } catch (err) {
-  return res.status(500).json({ status: "error", error: "Upstream or server failure"});
+    console.log("Error fetching data from genderize.io:", err);
+  return res.status(500).json({ status: "error", message: "Upstream or server failure"});
   }
 });
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ 
+    status: "error", 
+    message: "Route not found" 
+  });
+});
+
  
 
 app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
