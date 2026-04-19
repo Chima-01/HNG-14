fe462aac44e7925423ee633f68ba6fd57d23fb9d
Chinemerem <divinechima76@gmail.com>
2026-04-19 20:53:33 +0100

    fix: update env key name

--- stage-1/supabase.ts
+++ stage-1/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // These come from your Supabase Project Settings > API
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.SUPABASE_ANON_KEY!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: { persistSession: false }
